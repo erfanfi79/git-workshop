21360b1cb89c20998f25c179d8d6176c93e1c326
erfanfi79 <erfanfaravani@gmail.com>
2022-11-11 23:53:37 +0330

    Add Routing to pages

---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Row } from "react-bootstrap";
+class Home extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Row>
+          <h1>home</h1>{" "}
+        </Row>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Home;
